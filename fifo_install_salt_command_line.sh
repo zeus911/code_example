523f006633d38345bd477d88e17022b24dfe9103
frank <publicwu@163.com>
2017-03-27 14:03:52 +0800

    pkgin-rsync

--- fifo_install_salt_command_line.sh
+++ fifo_install_salt_command_line.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 set -e
+
+#salt -L  'home_fifo,home_fifo2'   cmd.run      'svcadm restart snarl '
+#salt -L  'home_fifo,home_fifo2'   cmd.run      'svcadm restart sniffle '
+#salt -L  'home_fifo,home_fifo2'   cmd.run      'svcadm restart howl'
+
+
 salt-run manage.down removekeys=True
 salt '*' saltutil.refresh_pillar
 salt home-smartos.wu state.sls_id   create_home_fifo2_vm        create_fifo_vm  -t 600
