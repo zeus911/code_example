8df678e35936988d009318c0f05be150bc9b5d29
frank <publicwu@163.com>
2017-04-06 10:30:38 +0800

    migrate-kvm-salt-again

--- chunter_install_salt.cmd.script.sh
+++ chunter_install_salt.cmd.script.sh
@@ -5,26 +5,26 @@ log_file_name=chunter_install_`date +%F-%H_%M`.log
 exec &> >(tee "/opt/$log_file_name")                 
 
         
-VERSION=rel
-cd /opt
-curl -O http://salt/chunter-fifo/fifo_zlogin-latest.gz 
-gunzip fifo_zlogin-latest.gz
-sh fifo_zlogin-latest
+#VERSION=rel
+#cd /opt
+#curl -O http://salt/chunter-fifo/fifo_zlogin-latest.gz 
+#gunzip fifo_zlogin-latest.gz
+#sh fifo_zlogin-latest
 
 
 VERSION=rel
 cd /opt
-curl -O http://10.0.1.38/fifo-0.7.0/chunter-0.7.0p4.gz
-gunzip chunter-latest.gz
-sh chunter-latest
+curl -O http://salt/fifo-0.7.0/chunter-0.7.0p4.gz
+gunzip chunter-0.7.0p4.gz
+sh chunter-0.7.0p4 
 
 
 svcadm enable epmd
 sleep 5
 svcs epmd
-svcadm enable fifo/zlogin
-sleep 5
-svcs fifo/zlogin
+#svcadm enable fifo/zlogin
+#sleep 5
+#svcs fifo/zlogin
 svcadm enable chunter
 sleep 5
 svcs chunter
