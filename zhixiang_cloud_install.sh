f65cef16954475a71af1cb91bba1dd652e76f3eb
frank <publicwu@163.com>
2017-06-25 21:19:30 +0800

    zhixiang_cloud_two_cluster

--- zhixiang_cloud_install.sh
+++ zhixiang_cloud_install.sh
@@ -29,9 +29,11 @@ salt '*' saltutil.refresh_pillar
 #salt jinhao      state.sls_id   create_new_fifo_home_1_vm          create_fifo_vm  -t 600
 #salt jinhao      state.sls_id   dataset_install_new_fifo_home_1    config_smartos_vm -t 3600
 
-
-salt honeymoon.honeymoon     state.sls_id   create_briphant_cloud_beijing_1_vm          create_fifo_vm  -t 600
-salt honeymoon.honeymoon     state.sls_id   dataset_install_briphant_cloud_beijing_1    config_smartos_vm -t 3600
+for fifo_zone_number in 1 2 
+do
+salt honeymoon.honeymoon     state.sls_id   create_briphant_cloud_beijing_"$fifo_zone_number"_vm          create_fifo_vm  -t 600
+salt honeymoon.honeymoon     state.sls_id   dataset_install_briphant_cloud_beijing_$fifo_zone_number    config_smartos_vm -t 3600
+done
 #升级配置数据
 #salt fifo_1_aio_9_1_home state.sls config_fifo_zone
 
@@ -44,16 +46,31 @@ salt honeymoon.honeymoon     state.sls_id   dataset_install_briphant_cloud_beiji
 #迁移数据
 #salt jinhao    cmd.script salt://script/transfer_data_in_fifo_zone.sh shell='/bin/bash'
 
-salt 'briphant_cloud_beijing_1' cmd.run "svcadm enable epmd;svcadm enable snarl; svcadm enable howl;svcadm enable sniffle"
+salt 'briphant_cloud_beijing_*' cmd.run "svcadm enable epmd;svcadm enable snarl; svcadm enable howl;svcadm enable sniffle"
 #salt 'fifo_*_aio_9_1_home' cmd.run "svcadm restart snarl; svcadm restart howl;svcadm restart sniffle"
 #salt 'fifo_*_aio_9_1_home' cmd.run "svcadm disable snarl; svcadm disable howl;svcadm disable sniffle"
 #salt 'fifo_*_aio_9_1_home' cmd.run "svcadm clear snarl; svcadm clear howl;svcadm clear sniffle"
 #salt 'fifo_*_aio_9_1_home' cmd.run "svcadm enable snarl; svcadm enable howl;svcadm enable sniffle"
-sleep 20
+sleep 10
+salt 'briphant_cloud_beijing_*' cmd.run "svcs epmd snarl sniffle howl"
+
+sleep 5
+          
+          #/opt/local/fifo-howl/bin/howl-admin cluster join 'howl@10.0.1.85'
+          #/opt/local/fifo-howl/bin/howl-admin cluster plan
+          #/opt/local/fifo-howl/bin/howl-admin cluster commit
+          #
+          #/opt/local/fifo-snarl/bin/snarl-admin cluster join 'snarl@10.0.1.85'
+          #/opt/local/fifo-snarl/bin/snarl-admin cluster plan
+          #/opt/local/fifo-snarl/bin/snarl-admin   cluster commit
+
 #salt 'briphant_cloud_beijing_1' cmd.run "snarl-admin init default MyOrg Users admin admin"  -t 600
 salt 'briphant_cloud_beijing_1' cmd.run "/opt/local/fifo-snarl/bin/snarl-init"  -t 600
 sleep 10;
-#salt 'briphant_cloud_beijing_1' cmd.run "sniffle-admin init-leofs 10.20.2.130.xip.io;svcs epmd snarl sniffle howl;sniffle-admin config show" 
+ 
+salt 'briphant_cloud_beijing_2' cmd.run          "sniffle-admin cluster join 'sniffle@10.20.2.86';sleep 15;sniffle-admin cluster plan;sleep 10;sniffle-admin cluster commit" -t 600
+salt 'briphant_cloud_beijing_2' cmd.run          "snarl-admin cluster   join 'snarl@10.20.2.86';sleep 15;snarl-admin cluster plan;sleep 10;snarl-admin cluster commit" -t 600
+salt 'briphant_cloud_beijing_2' cmd.run          "howl-admin cluster    join 'howl@10.20.2.86';sleep 15;howl-admin cluster plan;sleep 10;howl-admin cluster commit" -t 600
 
  salt 'briphant_cloud_beijing_1' cmd.run "sniffle-admin    config set       storage.s3.access_key                               4218d530b9ff5b78214f" 
  salt 'briphant_cloud_beijing_1' cmd.run "sniffle-admin    config set       storage.s3.secret_key           5130e6a33a8b1db9b39bf2c4437eec7fb3d9b04c" 
@@ -61,17 +78,19 @@ sleep 10;
  salt 'briphant_cloud_beijing_1' cmd.run "sniffle-admin    config set     storage.s3.image_bucket                                        fifo-images" 
  salt 'briphant_cloud_beijing_1' cmd.run "sniffle-admin    config set  storage.s3.snapshot_bucket                                       fifo-backups"
  salt 'briphant_cloud_beijing_1' cmd.run "sniffle-admin config show" 
-
+ #salt 'briphant_cloud_beijing_1' cmd.run "sniffle-admin init-leofs 10.20.2.130.xip.io;svcs epmd snarl sniffle howl;sniffle-admin config show"
 sleep 10
 #rsyslog
-salt 'briphant_cloud_beijing_1'  state.sls fifo_rsyslog7_conf_blockreplace
+
+salt 'briphant_cloud_beijing_*'  state.sls fifo_rsyslog7_conf_blockreplace
 salt 'briphant_cloud_beijing_1' cmd.run "ls -ltr /data/*/log/"  shell='/bin/bash'
-salt 'briphant_cloud_beijing_1' cmd.run "svcs epmd snarl sniffle howl"
-sleep 20
+
+
+
+
+salt 'briphant_cloud_beijing_*'  state.single      file.managed  template=jinja name='/opt/local/etc/nginx/nginx.conf'   source='salt://file/nginx.conf.zhixiang_cloud'  backup='minion'
+salt 'briphant_cloud_beijing_*'  state.single      file.managed  template=jinja name=' /opt/local/etc/nginx/includes/flower.conf'   source='salt://file/flower.conf.zhixiang_cloud'  backup='minion'
+salt 'briphant_cloud_beijing_*'  cmd.run           "svcadm enable nginx"
+sleep 30
 salt 'briphant_cloud_beijing_1' cmd.run "howl-admin status;sniffle-admin status;snarl-admin status;"
 salt 'briphant_cloud_beijing_1' cmd.run "snarl-admin member-status;howl-admin member-status;sniffle-admin member-status"
-
-
-salt 'briphant_cloud_beijing_1'  state.single      file.managed  template=jinja name='/opt/local/etc/nginx/nginx.conf'   source='salt://file/nginx.conf.zhixiang_cloud'  backup='minion'
-salt 'briphant_cloud_beijing_1'  state.single      file.managed  template=jinja name=' /opt/local/etc/nginx/includes/flower.conf'   source='salt://file/flower.conf.zhixiang_cloud'  backup='minion'
-salt 'briphant_cloud_beijing_1'  cmd.run           "svcadm enable nginx"
