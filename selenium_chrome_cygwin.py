6f2bc61d11a45bb4bb4ff5ab79ebe5b0071665a8
frank wu <unarywu@163.com>
2018-03-13 10:02:27 +0800

    selenium-chrome-cygwin

---
+++ selenium_chrome_cygwin.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+import os
+import sys
+sys.path.insert(0, "/cygdrive/c/python27/lib/site-packages/")
+from selenium import webdriver
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoAlertPresentException
+import unittest, time, re
+
+class UntitledTestCase(unittest.TestCase):
+    def setUp(self):
+
+
+        options = webdriver.ChromeOptions()
+        options.add_argument("download.default_directory=/tmp")
+
+        self.driver = webdriver.Chrome(chrome_options=options)
+        self.driver.implicitly_wait(30)
+        self.base_url = "https://www.katalon.com/"
+        self.verificationErrors = []
+        self.accept_next_alert = True
+
+    def test_untitled_test_case(self):
+        driver = self.driver
+        driver.get("http://23.102.236.94:4200/login")
+        driver.find_element_by_name("username").click()
+        driver.find_element_by_name("username").clear()
+        driver.find_element_by_name("username").send_keys("Dev1admin")
+        driver.find_element_by_name("password").clear()
+        driver.find_element_by_name("password").send_keys("111111")
+        driver.find_element_by_xpath("//app-at-login/div").click()
+        driver.find_element_by_xpath("//button[@type='button']").click()
+        time.sleep(10)
+        driver.find_element_by_link_text("Export").click()
+        driver.find_element_by_xpath("//div[2]/div[2]/mat-icon").click()
+        time.sleep(20)
+    def is_element_present(self, how, what):
+        try: self.driver.find_element(by=how, value=what)
+        except NoSuchElementException as e: return False
+        return True
+
+    def is_alert_present(self):
+        try: self.driver.switch_to_alert()
+        except NoAlertPresentException as e: return False
+        return True
+
+    def close_alert_and_get_its_text(self):
+        try:
+            alert = self.driver.switch_to_alert()
+            alert_text = alert.text
+            if self.accept_next_alert:
+                alert.accept()
+            else:
+                alert.dismiss()
+            return alert_text
+        finally: self.accept_next_alert = True
+
+    def tearDown(self):
+        self.driver.quit()
+        self.assertEqual([], self.verificationErrors)
+
+if __name__ == "__main__":
+    unittest.main()
