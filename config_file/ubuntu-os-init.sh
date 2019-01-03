687f61b53119e6cbe03f63a6f06f48adb4ed3480
frank wu <unarywu@163.com>
2019-01-03 11:28:50 +0800

    ubuntu init bug2

--- config_file/ubuntu-os-init.sh
+++ config_file/ubuntu-os-init.sh
@@ -1,7 +1,7 @@
 apt-get update
 apt-get -y install proxychains
 cp /etc/proxychains.conf /etc/proxychains.conf.$(date "+%b_%d_%Y_%H.%M.%S")
-sed 's/socks4  127.0.0.1 8000/socks5  127.0.0.1 1080/' /usr/local/etc/proxychains.conf
+sed 's/socks4  127.0.0.1 8000/socks5  127.0.0.1 1080/' /etc/proxychains.conf
 proxychains4 apt-get -y install tree lrzsz dos2unix build-essential autoconf telnet screen vim  dnsutils tftp git openssh-server aria2 autossh proxychains curl iotop zsh zssh
 proxychains4 curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh 
     cp ~/.zshrc ~/.zshrc.bak.$(date "+%b_%d_%Y_%H.%M.%S")
