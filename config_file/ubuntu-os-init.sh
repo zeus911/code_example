ac5e3534b8441810ef3de89f59184684f31b1839
frank wu <unarywu@163.com>
2019-01-04 11:47:01 +0800

    os-init

--- config_file/ubuntu-os-init.sh
+++ config_file/ubuntu-os-init.sh
@@ -2,10 +2,10 @@ apt-get update
 apt-get -y install proxychains
 cp /etc/proxychains.conf /etc/proxychains.conf.$(date "+%b_%d_%Y_%H.%M.%S")
 sed -i 's/socks4  127.0.0.1 8000/socks5  127.0.0.1 1080/' /etc/proxychains.conf
-proxychains apt-get -y install tree lrzsz dos2unix build-essential autoconf telnet screen vim  dnsutils tftp git openssh-server aria2 autossh proxychains curl iotop zsh zssh
+proxychains apt-get -y install tree lrzsz dos2unix build-essential autoconf telnet screen vim  dnsutils tftp git openssh-server aria2 autossh proxychains curl iotop zsh zssh nmap
 proxychains curl -o ./zsh_install.sh -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh 
     chmod +x  ./zsh_install.sh
     ./zsh_install.sh
     cp ~/.zshrc ~/.zshrc.bak.$(date "+%b_%d_%Y_%H.%M.%S")
-    sed -i  's/^  git/  history dircycle/' ~/.zshrc
+    sed -i  's/^  git/  history dircycle systemd/' ~/.zshrc
     sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="afowler"/'  ~/.zshrc
