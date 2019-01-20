897ceafab295520817337cbe71653d6ac5da5b62
wujunrong <publicwu@163.com>
2019-01-20 14:32:49 +0800

    os-init-sudo

--- config_file/ubuntu-os-init.sh
+++ config_file/ubuntu-os-init.sh
@@ -1,8 +1,9 @@
-#sed s/^proxychains//g ./ubuntu-os-init.sh
-apt-get update
-apt-get -y install proxychains
-cp /etc/proxychains.conf /etc/proxychains.conf.$(date "+%b_%d_%Y_%H.%M.%S")
-sed -i 's/socks4  127.0.0.1 8000/socks5  127.0.0.1 1080/' /etc/proxychains.conf
+#sed -i s/^proxychains//g ./ubuntu-os-init.sh
+sudo sh -c  'echo "frank ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/frank_sudo'
+sudo apt-get update
+sudo apt-get -y install proxychains
+sudo cp /etc/proxychains.conf /etc/proxychains.conf.$(date "+%b_%d_%Y_%H.%M.%S")
+sudo sed -i 's/socks4  127.0.0.1 8000/socks5  127.0.0.1 1080/' /etc/proxychains.conf
 proxychains apt-get -y install tree lrzsz dos2unix build-essential autoconf telnet screen vim  dnsutils tftp git openssh-server aria2 autossh proxychains curl iotop zsh zssh nmap
 proxychains curl -o ./zsh_install.sh -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh 
     chmod +x  ./zsh_install.sh
