9d052e60dc6cc834cdcb30523f3d653299f9a523
frank <unarywu@gimail.com>
2019-01-21 15:33:19 +0800

    centos-init-proxychain4

--- config_file/centos-os-init.sh
+++ config_file/centos-os-init.sh
@@ -18,8 +18,8 @@ sudo make install-config
 cp /usr/local/etc/proxychains.conf /usr/local/etc/proxychains.conf.$(date "+%b_%d_%Y_%H.%M.%S")
 sudo sed -i 's/9050/1080/g' /usr/local/etc/proxychains.conf
 
-proxychains yum -y install tftp epel-release wim wget curl telnet lrzsz aria2 openssh-clients openssh-server iotop net-tools lsof tcpdump tree tcpdump zip unzip less dos2unix git iproute bind-utils autossh iproute proxychains zsh zssh vim nmap
-proxychains curl -o ./zsh_install.sh -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh 
+proxychains4 yum -y install tftp epel-release wim wget curl telnet lrzsz aria2 openssh-clients openssh-server iotop net-tools lsof tcpdump tree tcpdump zip unzip less dos2unix git iproute bind-utils autossh iproute proxychains zsh zssh vim nmap
+proxychains4 curl -o ./zsh_install.sh -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh 
     chmod +x  ./zsh_install.sh
     ./zsh_install.sh
 
