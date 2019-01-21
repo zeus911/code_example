41171d1234ad9207d75fc69f9bd8dddfef538c4c
frank <unarywu@gimail.com>
2019-01-21 11:52:52 +0800

    zsh-font

--- config_file/config-zsh.sh
+++ config_file/config-zsh.sh
@@ -4,10 +4,18 @@
     #sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="afowler"/'  ~/.zshrc
     
     sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="agnoster"/'  ~/.zshrc
+    
+    #install font method1:
     #https://medium.com/@Andreas_cmj/how-to-setup-a-nice-looking-terminal-with-wsl-in-windows-10-creators-update-2b468ed7c326
     #on windows10 download and doble click to  exec ttf file,and change wsl terminal's font to DejaVu Sans Mono for Powerline
     #https://github.com/powerline/fonts/blob/master/DejaVuSansMono/DejaVu%20Sans%20Mono%20for%20Powerline.ttf
     
+    #install font method2:
+    git clone git@github.com:powerline/fonts.git
+    cd fonts
+    ./install.sh
+     #iterm2->Preferences->Profiles->Text->Font->Change Font->Droid Sans Mono for Powerline
+
     cd ~/.oh-my-zsh/custom/plugins
     git clone https://github.com/zsh-users/zsh-autosuggestions
 
